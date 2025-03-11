2e1ce9ba3b9ddf379147c3797f4ced62bb313572
Yaroslav Matusevich <yari.matusevich@gmail.com>
2025-03-11 17:54:28 -0400

    Integrated Gemini model into extension

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,13 @@
 import OnClickData = chrome.contextMenus.OnClickData;
+import {GoogleGenerativeAI} from "@google/generative-ai";
 
+// Imports generative AI model from Gemini API using key
+const genAI = new GoogleGenerativeAI("API KEY");
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+
+/**
+ * Upon installation of extension creates a context menu item for summarization
+ */
 chrome.runtime.onInstalled.addListener(function() {
     // Creating the context menu item
     chrome.contextMenus.create({
@@ -10,16 +18,28 @@ chrome.runtime.onInstalled.addListener(function() {
 
     console.log("Created context menu")
 
-    // Create listener for user clicks, invokes saveSelection
-    chrome.contextMenus.onClicked.addListener(saveSelection);
+    // Create listener for user clicks, invokes promptWithCurrentSelection
+    chrome.contextMenus.onClicked.addListener(promptWithCurrentSelection);
 });
 
-function saveSelection(info: OnClickData) {
-    if (info == null) {
+async function promptWithCurrentSelection(info: OnClickData) {
+    // Check to see if current selection is defined
+    if (info.selectionText == null) {
         console.log("Error: OnClickData is null");
         return;
     }
 
-    const currentSelection = info.selectionText;
-    console.log("Current selection: " + currentSelection);
+    // Prompt sent to Gemini
+    const prompt = "Summarize this: " + info.selectionText;
+
+    // Response from Gemini to prompt
+    const result = await model.generateContent(prompt);
+
+    const candidate = result.response.candidates?.[0] ?? null;
+
+    if (candidate == null) {
+        return;
+    } else {
+        console.log("Summary: " + candidate.content.parts[0].text);
+    }
 }
