05fb4ef5ab9c695a8ff949b36e6c69feb25011b8
Yaroslav Matusevich <yari.matusevich@gmail.com>
2025-03-11 22:51:50 -0400

    Created input field for user API keys

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  return <div>
+      <WelcomeMessage />
+      <KeyInputForm />
+  </div>
+}
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function WelcomeMessage() {
+    return <div>
+        <h1> SummarizeThis </h1>
+        <p> Input your Gemini API key to begin summarizing! </p>
+    </div>
+}
+
+function KeyInputForm() {
+    const [key, setKey] = useState('');
+
+    const handleKeySubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        console.log("Submitted Key:" + key);
+    };
+
+    return <form onSubmit = {handleKeySubmit}>
+        <label> API Key: </label>
+        <input type = 'text' value = {key} onChange =
+            {e => setKey(e.target.value)} />
+        <button type = "submit"> Submit </button>
+    </form>
 }
 
 export default App
